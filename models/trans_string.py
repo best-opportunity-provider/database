a3fb1c60c3369df7e67f74df83252485494d2c8d
teviroff <mkaluzhnyy@mail.ru>
2025-02-07 02:44:07 +0300

    added pydantic & created 	rans_string deserializer model

--- models/trans_string.py
+++ models/trans_string.py
@@ -1,5 +1,8 @@
+from typing import Self
 from enum import StrEnum
 import mongoengine as mongo
+import pydantic
+from pydantic_core import PydanticCustomError
 
 
 class Language(StrEnum):
@@ -8,15 +11,45 @@ class Language(StrEnum):
 
 
 class TransStringData(mongo.EmbeddedDocument):
-    """Auxillary model for `TransString`. One benefit of such structure is ability to embed string directly 
-       on owner side and not in a separate collection.
-
-       Contains all possible language variants, but none are required.
+    """Auxillary model for `TransString`, containing actual translated strings. 
+    
+       One benefit of such structure is ability to embed string directly on owner side 
+       and not in a separate collection.
     """
 
     fallback_language = mongo.EnumField(Language)
     en = mongo.StringField()
     ru = mongo.StringField()
+
+    # TODO: reconsider naming
+    class Deserializer(pydantic.BaseModel):
+        model_config = pydantic.ConfigDict(
+            extra='ignore',
+        )
+
+        fallback_language: Language | None = None
+        en: str | None = None
+        ru: str | None = None
+
+        @pydantic.model_validator(mode='after')
+        def check_non_empty(self) -> Self:
+            if all(getattr(self, language.value) is None for language in Language):
+                raise PydanticCustomError(
+                    'empty-trans-string',
+                    'Translation string contains no data',
+                )
+            return self
+
+        @pydantic.model_validator(mode='after')
+        def check_valid_fallback(self) -> Self:
+            if self.fallback_language is not None and \
+               getattr(self, self.fallback_language.value) is None:
+                raise PydanticCustomError(
+                    'missing-fallback-trans',
+                    'Missing fallback language translation',
+                    { 'fallback': self.fallback_language, },
+                )
+            return self
 
 
 class TransString(mongo.Document):
